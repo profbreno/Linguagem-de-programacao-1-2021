025045443e81fccaac929fb636c06ae749044c35
profbreno <brenod123@gmail.com>
2021-09-14 10:21:36 -0300

    Inclusão de um comentário

--- src/App.java
+++ src/App.java
@@ -2,24 +2,24 @@
 public class App {
 
 	public static void main(String[] args) {
-		
+
+		// Instâncias do objeto
 		Contador contador = new Contador();
 		Contador contador2 = new Contador();
-		
+
 		contador.atribuirValor(73);
 		contador2.atribuirValor(199);
 		int valorAtual = contador.getValorAtual();
 		System.out.println("Valor inicial: " + valorAtual);
 		contador.incrementarValor();
 		valorAtual = contador.getValorAtual();
-		System.out.println("Valor ap�s o incremento: " + valorAtual);
-		
+		System.out.println("Valor após o incremento: " + valorAtual);
+
 		int valor = contador2.getValorAtual();
-		System.out.println("Valor da segunda inst�ncia: "+ valor);
+		System.out.println("Valor da segunda instância: " + valor);
 		contador2.incrementarValor();
 		valor = contador2.getValorAtual();
-		System.out.println("Valor da segunda inst�ncia ap�s o incremento: " 
-				+ valor);
-		
+		System.out.println("Valor da segunda instância após o incremento: " + valor);
+
 	}
 }
